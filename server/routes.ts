0d54169c8753164405493173ba4abbe648d07d46
bilalkhalidshaikh <muhammadbilalkhalid56@gmail.com>
2025-10-29 03:56:18 +0500

    fix: alias fallback for Vercel build

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import { insertBookingSchema } from "@shared/schema";
+import { insertBookingSchema } from "../shared/schema";
 import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
 
